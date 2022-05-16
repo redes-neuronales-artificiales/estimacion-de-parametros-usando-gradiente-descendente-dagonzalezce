1e6a48641a76aa532c9e06a874b5d28d6a01dfd3
David González <dagonzalezce@unal.edu.co>
2022-05-15 21:18:17 -0500

    Submission

--- preguntas.py
+++ preguntas.py
@@ -13,19 +13,19 @@ def pregunta_01():
     Complete el código presentado a continuación.
     """
     # Importe pandas
-    import ___ as ___
+    import pandas as pd
 
     # Importe PolynomialFeatures
-    from ___ import ___
+    from sklearn.preprocessing import PolynomialFeatures
 
     # Cargue el dataset `data.csv`
-    data = ___.___("___")
+    data = pd.read_csv('data.csv')
 
     # Cree un objeto de tipo `PolynomialFeatures` con grado `2`
-    poly = ___.___(___)
+    poly = PolynomialFeatures(2)#___.___(___)
 
     # Transforme la columna `x` del dataset `data` usando el objeto `poly`
-    x_poly = poly.___(data[["___"]])
+    x_poly = poly.transform(data[["x"]])
 
     # Retorne x y y
     return x_poly, data.y
